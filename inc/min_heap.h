110bbb4baf53def9d5e6e880d28e803f690e2233
xiaohy <kzxiao.hy@gmail.com>
2015-10-29 21:25:14 +0800

    doing heap

---
+++ inc/min_heap.h
@@ -0,0 +1,12 @@
+#pragma once
+
+/**
+ * heap[0] is heap size
+ * 2            1           10
+ * |            |
+ * heap size    heap root
+ */
+
+void min_heap_init(int* heap);
+void min_heap_insert(int heap, int val);
+void min_heap_pop(int* heap);
---
+++ src/min_heap.c
@@ -0,0 +1,11 @@
+#include "min_heap.h"
+
+#include "stddef.h"
+
+void min_heap_init(int* heap)
+{
+    for(size_t i = (heap[0] >> 1); i > 0; --i)
+    {
+
+    }
+}
