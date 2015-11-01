bf0394810dca6e585f2f20f74a8a638229aef3b3
XT1024 <286404108@qq.com>
2015-11-01 21:13:26 +0800

    add ttyo

--- src/panic.c
+++ src/panic.c
@@ -4,13 +4,13 @@
 
 void panic(const char* str)
 {
-    printk("\x1b\x0c%s", str);
+    printk("\033[32m%s", str);
     while(1);
 }
 
 void _static_assert_func(const char* name, const char* file, unsigned int line, const char* func)
 {
-    printk("\x1b\x0c""assertion \"%s\" failed: file \"%s\""
+    printk("\033[32;35;31m\x0c""assertion \"%s\" failed: file \"%s\""
             ", line %u, function: %s", name, file, line, func);
     while(1);
 }
