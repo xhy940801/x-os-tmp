3e36d70ce2b07fe5db2a7f263e01c39aea2e4510
xiaohy <kzxiao.hy@gmail.com>
2015-11-02 20:23:25 +0800

    doing level bitmap

--- inc/level_bitmap.h
+++ inc/level_bitmap.h
@@ -1,9 +1,17 @@
 #pragma once
 
 #include "stdint.h"
+#include "stddef.h"
 
 struct level_bitmap_t
 {
     uint32_t* bitmaps;
-    uint32_t max_level;
+    size_t max_level;
 };
+
+int level_bitmap_bit_test(struct level_bitmap_t* map, size_t bit);
+int level_bitmap_bit_set(struct level_bitmap_t* map, size_t bit);
+int level_bitmap_bit_clear(struct level_bitmap_t* map, size_t bit);
+ssize_t level_bitmap_get_min(struct level_bitmap_t* map);
+ssize_t level_bitmap_get_max(struct level_bitmap_t* map);
+void level_bitmap_cpy(struct level_bitmap_t* dst, struct level_bitmap_t* src);
--- src/level_bitmap.c
+++ src/level_bitmap.c
@@ -1,1 +1,63 @@
 #include "level_biimap.h"
+
+static const size_t level_jump_sizes[] = {
+    0,
+    ((32 << 0) - 1) / (32 - 1),
+    ((32 << 5) - 1) / (32 - 1),
+    ((32 << 10) - 1) / (32 - 1),
+    ((32 << 15) - 1) / (32 - 1),
+    ((32 << 20) - 1) / (32 - 1),
+    ((32 << 25) - 1) / (32 - 1),
+    ((32 << 30) - 1) / (32 - 1),
+};
+
+int level_bitmap_bit_test(struct level_bitmap_t* map, size_t bit)
+{
+    size_t npos = bit & 31;
+    size_t n = 0;
+    while(bit)
+    {
+        bit >>= 5;
+        n += bit;
+    }
+    n += map->max_level;
+    return (map->bitmap[n]) & (1 << npos);
+}
+
+int level_bitmap_bit_set(struct level_bitmap_t* map, size_t bit)
+{
+    size_t i;
+    unsigned int* p = map->bitmaps;
+    for(i = 0; i < map->max_level; ++i)
+    {
+        size_t npos = bit & 31;
+        *p &= (1 << npos);
+        p += npos * level_jump_sizes[map->max_level - i] + 1;
+        bit >> 5;
+    }
+    size_t npos = bit &= 31;
+    int rs = (*p) & (1 << npos);
+    *p |= (1 << npos);
+    return rs;
+}
+
+//TODO
+int level_bitmap_bit_clear(struct level_bitmap_t* map, size_t bit)
+{
+    size_t npos = bit & 31;
+    size_t n = 0;
+    while(bit)
+    {
+        bit >>= 5;
+        n += bit;
+    }
+    n += map->max_level;
+    int rs = (map->bitmap[n]) & (1 << npos);
+    map->bitmap[n] &= (~(1 << npos));
+    return rs;
+}
+
+ssize_t level_bitmap_get_min(struct level_bitmap_t* map)
+{
+
+}
